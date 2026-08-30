33976d7a0b324c211b645b62e482a99283b77a2b
babyganesh <babyganeshece@gmail.com>
2026-08-30 16:44:44 +0530

    basic task python

--- python_demo.py
+++ python_demo.py
@@ -40,8 +40,17 @@
 # print(f,type(f),id(f))
 
 a=[10,30,40,50]
-b=a
-c=[10,30,40,50]
-print(a is b,b!=c)          # is and is not discribe location of value .like true or false
-print(c is not b,a is c)
-print(b==a)
+# b=a
+# c=[10,30,40,50]
+# print(a is b,b!=c)          # is and is not discribe location of value .like true or false
+# print(c is not b,a is c)
+# print(b==a)
+
+
+
+
+
+
+
+
+
